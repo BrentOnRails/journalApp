4e791036406a7c38d9eb27cd72e9a6a719bf32df
Brent Smith <brentalansmith@gmail.com>
2014-03-11 17:57:17 -0700

    done bitches

--- app/assets/javascripts/views/posts/post_show.js
+++ app/assets/javascripts/views/posts/post_show.js
@@ -9,6 +9,27 @@ Journal.Views.PostShow = Backbone.View.extend({
     return this;
   },
 
+  events: {
+    "dblclick #title" : "changeThingy",
+    "dblclick #body" : "changeThingy",
+    "blur input" : "submit"
+  },
 
+  changeThingy: function(event) {
+    $node = $(event.currentTarget);
+    var id = $node.attr("id")
+    $node.html( ' <input id='+id+' >')
+  },
+
+  submit: function (event){
+    var $target = $(event.currentTarget)
+    var input = $target.val();
+    this.model.set($target.attr("id"), input)
+    this.model.save();
+    this.render();
+  },
 
 });
+
+
+
